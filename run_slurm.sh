08d7a358d69576e3b579a6221756282cb468a464
isaacvock <isaac.vock@gmail.com>
2023-08-29 17:19:47 -0400

    Increased default amount of time for full pipeline run

--- run_slurm.sh
+++ run_slurm.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=day
 #SBATCH --job-name=PROseq
 #SBATCH --cpus-per-task=1
-#SBATCH --time=10:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=your.email@yale.edu
 
