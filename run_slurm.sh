553926c31522c1317715fe31378f1f7c4c51d254
isaacvock <isaac.vock@gmail.com>
2024-09-10 09:28:04 -0400

    Add useful example

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=day
-#SBATCH --job-name=PROseq
+#SBATCH --job-name=fastq2EZbakR
 #SBATCH --cpus-per-task=1
-#SBATCH --time=12:00:00
+#SBATCH --time=23:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=your.email@yale.edu
 
@@ -15,3 +15,7 @@ conda activate deploy_snakemake
 conda config --set channel_priority strict
 
 snakemake --profile yale_profile/ --rerun-triggers mtime
+
+### Example for how to request rule-specific resources
+## Will request 180 GB for the rule named "index" (alignment index creation) and 12 hours for the rule named "align" (read alignment)
+# snakemake --profile yale_profile/ --rerun-triggers mtime --set-resources index:mem_mb=180000 align:time=12:00:00
